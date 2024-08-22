7a95117703a40c3065df2ee739a30251433d5f99
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2024-08-22 15:58:16 -0300

    normalizacao_padronizacao.py finalizado

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ## Intro_preparacao_dados.py
 Este arquivo é dedicado ao tratamento de dados existentes, incluindo a análise de duplicatas, 
 identificação de dados únicos, e a apresentação de estatísticas detalhadas sobre o conjunto de dados. 
+## Normalizacao_padronizacao.py
+Dedicado ao uso do Scikit-learn para a normalização e tratamento de dados, onde utilizamos as colunas "salário" e "idade" para comparar os 
+resultados obtidos com os métodos RobustScaler, MinMaxScaler e StandardScaler.
---
+++ normalizacao_padronizacao.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import RobustScaler, MinMaxScaler, StandardScaler
+
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+df = pd.read_csv('clientes-v2-tratados.csv')
+
+print(df.head())
+
+df = df.drop(['data', 'estado', 'nivel_educacao','numero_filhos', 'estado_civil', 'area_atuacao'], axis=1)
+
+# Normalizacao - MinMaxScaler
+scaler = MinMaxScaler()
+df['idadeMinMaxScaler'] = scaler.fit_transform(df[['idade']])
+df['salarioMinMaxScaler'] = scaler.fit_transform(df[['salario']])
+
+min_max_scaler = MinMaxScaler(feature_range=(-1, 1))
+df['idadeMinMaxScaler_mm'] = min_max_scaler.fit_transform(df[['idade']])
+df['salarioMinMaxScaler_mm'] = min_max_scaler.fit_transform(df[['salario']])
+
+# Padronizacao - StandardScaler
+scaler = StandardScaler()
+df['idadeStandardScaler'] = scaler.fit_transform(df[['idade']])
+df['salarioStandardScaler'] = scaler.fit_transform(df[['salario']])
+
+# Padronizacao - RobustScaler
+scaler = RobustScaler()
+df['idadeRobustScaler'] = scaler.fit_transform(df[['idade']])
+df['salarioRobustScaler'] = scaler.fit_transform(df[['salario']])
+
+print(df.head(15))
